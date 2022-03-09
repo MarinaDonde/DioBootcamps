ca10f9432b2eb81cb1c6f64911987b250076059f
Marina Donde <madondee@hotmail.com>
2022-03-08 22:59:29 -0300

    Padroes Projeto Java

---
+++ PadroesProjetoJava/padroesProjetoJava/src/one/digitalinnovation/gof/subsistema2/cep/CepApi.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.subsistema2.cep;
+
+public class CepApi {
+    private static CepApi instacia = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+
+    public static CepApi getInstacia() {
+        return instacia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Belo Horizonte";
+    }
+    public String recuperarEstado(String cep) {
+        return "MG";
+    }
+}
