755e68596966506bbb65a3b0f985be0f00809980
Marina Donde <madondee@hotmail.com>
2022-03-08 22:47:50 -0300

    Desafio de Orientação a Objeto

---
+++ DesafioPOO/src/desafioPOO/Curso.java
@@ -0,0 +1,31 @@
+package desafioPOO;
+
+public class Curso extends Conteudo{
+	
+	private String titulo;
+	private String descricao;
+	private int cargaHoraria;
+
+	
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	
+	@Override
+	public String toString() {
+		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+	
+	@Override
+	public double calcularXp() {
+
+		return xpPadrao * cargaHoraria;
+	}
+	
+	
+	
+}
